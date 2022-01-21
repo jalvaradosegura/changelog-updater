da0377a5683fb28b3f0dc67afa216ba76bae2db1
Jorge Alvarado <alvaradosegurajorge@gmail.com>
2022-01-21 01:12:54 -0300

    Create a function to prepend git commit titles

--- changelog_updater/main.py
+++ changelog_updater/main.py
@@ -15,3 +15,9 @@ def get_git_commits_title() -> List[str]:
 
 def prepend_a_line_to_text(text: str, line: str) -> str:
     return f"{line}\n{text}"
+
+
+def prepend_git_commits_title(text: str) -> str:
+    commits_title = get_git_commits_title()
+    commits_title_as_text = "\n".join(commits_title)
+    return prepend_a_line_to_text(text, commits_title_as_text)
--- tests/changelog_updater_test.py
+++ tests/changelog_updater_test.py
@@ -1,6 +1,7 @@
 from changelog_updater.main import (
     get_git_commits_title,
     prepend_a_line_to_text,
+    prepend_git_commits_title,
 )
 
 
@@ -16,3 +17,19 @@ def test_get_git_commits_title():
     assert "Create the project" in result
     assert "Create a function to prepend text" == result[-2]
     assert "Create the project" == result[-1]
+
+
+def test_add_git_commits_title_to_text():
+    file_content = "* 0.1.0\n" "some text\n" "some more text\n"
+    expected_content = (
+        "Create a function to read git logs\n"
+        "Create a function to prepend text\n"
+        "Create the project\n"
+        "* 0.1.0\n"
+        "some text\n"
+        "some more text\n"
+    )
+
+    result = prepend_git_commits_title(file_content)
+
+    assert result == expected_content
