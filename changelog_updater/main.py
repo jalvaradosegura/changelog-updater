3a454ca2a5d29f986da6302f45ff619e3ebc85b2
Jorge Alvarado <alvaradosegurajorge@gmail.com>
2022-01-21 00:43:51 -0300

    Create a function to read git logs

--- changelog_updater/main.py
+++ changelog_updater/main.py
@@ -1,2 +1,17 @@
+import subprocess
+from typing import List
+
+
+def get_git_commits_title() -> List[str]:
+    result = (
+        subprocess.check_output(
+            ["git", "log", "--pretty=format:%s"], stderr=subprocess.STDOUT
+        )
+        .decode("utf-8")
+        .split("\n")
+    )
+    return result
+
+
 def prepend_a_line_to_text(text: str, line: str) -> str:
     return f"{line}\n{text}"
--- tests/changelog_updater_test.py
+++ tests/changelog_updater_test.py
@@ -1,7 +1,18 @@
-from changelog_updater.main import prepend_a_line_to_text
+from changelog_updater.main import (
+    get_git_commits_title,
+    prepend_a_line_to_text,
+)
 
 
-def test_prepend_a_line():
+def test_prepend_a_line_to_text():
     text = "Some random text"
     result = prepend_a_line_to_text(text, "Hi,")
     assert result == f"Hi,\n{text}"
+
+
+def test_get_git_commits_title():
+    result = get_git_commits_title()
+    assert "Create a function to prepend text" in result
+    assert "Create the project" in result
+    assert "Create a function to prepend text" == result[-2]
+    assert "Create the project" == result[-1]
