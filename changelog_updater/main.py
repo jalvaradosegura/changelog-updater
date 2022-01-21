7cd4c33a3c87a23a2005187641c429929f7f57a2
Jorge Alvarado <alvaradosegurajorge@gmail.com>
2022-01-21 01:41:02 -0300

    Fix tests

--- changelog_updater/main.py
+++ changelog_updater/main.py
@@ -2,7 +2,7 @@ import subprocess
 from typing import List
 
 
-def get_git_commits_title() -> List[str]:
+def get_all_commits_title() -> List[str]:
     result = (
         subprocess.check_output(
             ["git", "log", "--pretty=format:%s"], stderr=subprocess.STDOUT
@@ -13,11 +13,13 @@ def get_git_commits_title() -> List[str]:
     return result
 
 
-def prepend_a_line_to_text(text: str, line: str) -> str:
-    return f"{line}\n{text}"
+def prepend_a_text_to_text(*, base_text: str, prepend_this: str) -> str:
+    return f"{prepend_this}\n{base_text}"
 
 
-def prepend_git_commits_title(text: str) -> str:
-    commits_title = get_git_commits_title()
+def prepend_commits_title(base_text: str, *, commits_amount: int) -> str:
+    commits_title = get_all_commits_title()[0:commits_amount]
     commits_title_as_text = "\n".join(commits_title)
-    return prepend_a_line_to_text(text, commits_title_as_text)
+    return prepend_a_text_to_text(
+        base_text=base_text, prepend_this=commits_title_as_text
+    )
--- tests/changelog_updater_test.py
+++ tests/changelog_updater_test.py
@@ -1,35 +1,47 @@
+from typing import List
+
+import pytest
+
 from changelog_updater.main import (
-    get_git_commits_title,
-    prepend_a_line_to_text,
-    prepend_git_commits_title,
+    get_all_commits_title,
+    prepend_a_text_to_text,
+    prepend_commits_title,
 )
+
+
+@pytest.fixture
+def all_commits() -> List[str]:
+    return get_all_commits_title()
 
 
 def test_prepend_a_line_to_text():
     text = "Some random text"
-    result = prepend_a_line_to_text(text, "Hi,")
+    result = prepend_a_text_to_text(base_text=text, prepend_this="Hi,")
     assert result == f"Hi,\n{text}"
 
 
-def test_get_git_commits_title():
-    result = get_git_commits_title()
-    assert "Create a function to prepend text" in result
-    assert "Create the project" in result
-    assert "Create a function to prepend text" == result[-2]
-    assert "Create the project" == result[-1]
+def test_get_all_git_commits_title(all_commits: List[str]):
+    latest_commit = all_commits.pop(0)
+    penultimate_commits = all_commits.pop(0)
+
+    result = get_all_commits_title()
+
+    assert latest_commit == result[0]
+    assert penultimate_commits == result[1]
 
 
-def test_add_git_commits_title_to_text():
+def test_add_git_commits_title_to_text(all_commits):
+    latest_commit = all_commits.pop(0)
+    penultimate_commits = all_commits.pop(0)
     file_content = "* 0.1.0\n" "some text\n" "some more text\n"
     expected_content = (
-        "Create a function to read git logs\n"
-        "Create a function to prepend text\n"
-        "Create the project\n"
+        f"{latest_commit}\n"
+        f"{penultimate_commits}\n"
         "* 0.1.0\n"
         "some text\n"
         "some more text\n"
     )
 
-    result = prepend_git_commits_title(file_content)
+    result = prepend_commits_title(file_content, commits_amount=2)
 
     assert result == expected_content
